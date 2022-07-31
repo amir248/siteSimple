64d8328bff182a7a0f84a284b81647bc81d7410c
chikchicly <chikchicly@gmail.com>
2022-07-31 10:57:57 +0700

    ok

--- parallax.js
+++ parallax.js
@@ -74,7 +74,6 @@ document.querySelector('body > main > header > div.imAges > strong:nth-child(1)'
 document.querySelector('body > main > header > div.imAges > strong:nth-child(2)').addEventListener('mousemove',()=>{
   showCoords(event); // event !!!
   // let top=+0;
-
   document.querySelector('body > main > header > div.imAges > strong:nth-child(2)').style.cssText=`
   position:relative;
   top:${parallaxPoz.top}px;
@@ -82,9 +81,7 @@ document.querySelector('body > main > header > div.imAges > strong:nth-child(2)'
   right:${parallaxPoz.right}px;
   bottom:${parallaxPoz.bottom}px;
   transition: all 1s linear;
-
   `;
-
 });
 document.querySelector('body > main > header > div.imAges > strong:nth-child(2)').addEventListener('mouseout',()=>{
     showCoords(event);
@@ -99,28 +96,164 @@ document.querySelector('body > main > header > div.imAges > strong:nth-child(2)'
   `;
 });
 
-let pos1=0,pos2=0,pos3=0,pos4=0;
-let v1=0.5,v2=0.4,v3=0.3,v4=0.2;
-let dv=1;
-let x;
 
-document.querySelector('body > main > header > div:nth-child(3) > p').style.cssText=`
-  // width:70px;
-  max-width:100%;
-`;
-function moveBg(){
-  document.querySelector('body > main > header > div.imAges > strong:nth-child(2)').style.cssText=`
-  // height:70px;
-    position:relative;
-    top:5px;
-    // width:70px;
-  `;
-  pos1+=v1*dv;
-  pos2+=v2*dv;
-  pos3+=v3*dv;
-  pos4+=v4*dv;
-  document.querySelector('body > main > header > div:nth-child(3) > p').style.backgroundPositionX=pos1+"px,"+pos2+"px,"+pos3+"px,"+pos4+"px,0px";
+
+
+const parallaxPoz2={// const более глобальный вариант
+  left : 0,
+  right : 0,
+  bottom : 0,
+  top : 0
 }
+console.log("paralaxTwo");
+
+function showCoords2(evt){
+  console.log(
+    "clientX value: " + evt.clientX + "\n" +
+    "clientY value: " + evt.clientY + "\n"
+  );
+  console.log(evt);
+  if(evt.clientY<268||evt.clientY>273){
+    console.log('TwoClient');
+    parallaxPoz2.top=+30;
+    parallaxPoz2.right=+0;
+    parallaxPoz2.left=+0;
+    parallaxPoz2.bottom=+0;
+  }else{
+    console.log("evt.elset");
+    parallaxPoz2.top=+0;
+    parallaxPoz2.right=+0;
+    parallaxPoz2.left=+0;
+    parallaxPoz2.bottom=+30;
+  }
+  if(evt.clientX<570||evt.clientX>3300){
+    console.log('Two_evt.clientX>XXX');
+    // parallaxPoz.top=+0;
+    parallaxPoz2.right=+30;
+    parallaxPoz2.left=+0;
+    // parallaxPoz.bottom=+0;
+  }else{
+    console.log("evt.elset");
+    // parallaxPoz.top=+0;
+    parallaxPoz2.right=+0;
+    // parallaxPoz.bottom=+0;
+    parallaxPoz2.left=+30;
+  }
+}
+
+document.querySelector('body > main > header > div.imAges > span').addEventListener('mousemove',()=>{
+  showCoords2(event); // event !!!
+  // let top=+0;
+  document.querySelector('body > main > header > div.imAges > span').style.cssText=`
+  position:relative;
+  top:${parallaxPoz2.top}px;
+  left:${parallaxPoz2.left}px;
+  right:${parallaxPoz2.right}px;
+  bottom:${parallaxPoz2.bottom}px;
+  transition: all 1s linear;
+  `;
+
+});
+document.querySelector('body > main > header > div.imAges > span').addEventListener('mouseout',()=>{
+    showCoords2(event);
+  document.querySelector('body > main > header > div.imAges > span').style.cssText=`
+  position:relative;
+  top:0;
+  lett:0;
+  right:0;
+  bottom:0;
+  transition: all 1s linear;
+  `;
+
+});
+
+document.querySelector('body > main > header > div.imAges > p').addEventListener('mousemove',()=>{
+  showCoords2(event); // event !!!
+  // let top=+0;
+  document.querySelector('body > main > header > div.imAges > p').style.cssText=`
+  position:relative;
+  top:${parallaxPoz2.top}px;
+  left:${parallaxPoz2.left}px;
+  right:${parallaxPoz2.right}px;
+  bottom:${parallaxPoz2.bottom}px;
+  transition: all 1s linear;
+  `;
+
+});
+document.querySelector('body > main > header > div.imAges > p').addEventListener('mouseout',()=>{
+    showCoords2(event);
+  document.querySelector('body > main > header > div.imAges > p').style.cssText=`
+  position:relative;
+  top:0;
+  lett:0;
+  right:0;
+  bottom:0;
+  transition: all 1s linear;
+  `;
+
+});
+
+//text
+document.querySelector('body > main > header > div:nth-child(3) > p').addEventListener('mousemove',()=>{
+  showCoords2(event); // event !!!
+  // let top=+0;
+  document.querySelector('body > main > header > div:nth-child(3) > p').style.cssText=`
+  position:relative;
+  top:${parallaxPoz2.top}px;
+  left:${parallaxPoz2.left}px;
+  right:${parallaxPoz2.right}px;
+  bottom:${parallaxPoz2.bottom}px;
+  transition: all 1s linear;
+  `;
+
+});
+document.querySelector('body > main > header > div:nth-child(3) > p').addEventListener('mouseout',()=>{
+    showCoords2(event);
+  document.querySelector('body > main > header > div:nth-child(3) > p').style.cssText=`
+  position:relative;
+  top:0;
+  lett:0;
+  right:0;
+  bottom:0;
+  transition: all 1s linear;
+  `;
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let pos1=0,pos2=0,pos3=0,pos4=0;
+// let v1=0.5,v2=0.4,v3=0.3,v4=0.2;
+// let dv=1;
+// let x;
+
+// document.querySelector('body > main > header > div:nth-child(3) > p').style.cssText=`
+//   // width:70px;
+//   max-width:100%;
+// `;
+// function moveBg(){
+//   document.querySelector('body > main > header > div.imAges > strong:nth-child(2)').style.cssText=`
+//   // height:70px;
+//     position:relative;
+//     top:5px;
+//     // width:70px;
+//   `;
+//   pos1+=v1*dv;
+//   pos2+=v2*dv;
+//   pos3+=v3*dv;
+//   pos4+=v4*dv;
+//   document.querySelector('body > main > header > div:nth-child(3) > p').style.backgroundPositionX=pos1+"px,"+pos2+"px,"+pos3+"px,"+pos4+"px,0px";
+// }
 
 //29/07/2022 workins day
 // let moveId;
